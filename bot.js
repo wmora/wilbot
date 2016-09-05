84f34b5f36f8a53af0c8877afbad6a6ebe748b5e
William Mora <william.r.mora@gmail.com>
2016-09-04 20:56:13 -0400

    validator module

--- bot.js
+++ bot.js
@@ -1,19 +1,12 @@
 'use strict'
 
-var express = require('express')
+var express = require('express'),
+  validator = require('./validator.js')
 var app = express()
-
-const VERIFY_TOKEN = process.env.VERIFY_TOKEN || ''
 
 app.set('port', (process.env.PORT || 5000))
 
-app.get('/webhook', function(request, response) {
-  if (request.query['hub.verify_token'] === VERIFY_TOKEN) {
-    response.send(request.query['hub.challenge'])
-  } else {
-    response.send('Error, wrong validation token')
-  }
-})
+app.get('/webhook', validator.validateWebhook)
 
 app.listen(app.get('port'), function() {
   console.log('Node app running on port ', app.get('port'))
---
+++ validator.js
@@ -0,0 +1,9 @@
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN || ''
+
+exports.validateWebhook = function (request, response) {
+  if (request.query['hub.verify_token'] === VERIFY_TOKEN) {
+    response.send(request.query['hub.challenge'])
+  } else {
+    response.send('Error, wrong validation token')
+  }
+}
