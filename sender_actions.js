f44206d89e63c38d63dfa6d0157ffd7dac2fde17
William Mora <william.r.mora@gmail.com>
2016-09-05 21:21:41 -0400

    syntax fix

--- sender_actions.js
+++ sender_actions.js
@@ -9,7 +9,7 @@ exports.markAsSeen = function(recipientId) {
     },
     sender_action: 'mark_seen'
   }
-  facebook.sendMessage(body, () {
+  facebook.sendMessage(body, () => {
     console.log('sent mark_seen')
   })
 }
