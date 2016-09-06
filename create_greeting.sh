e0274a7f86a5135c941f175635599b1c948697c6
William Mora <william.r.mora@gmail.com>
2016-09-06 12:11:41 -0400

    properly escape single quote

--- create_greeting.sh
+++ create_greeting.sh
@@ -1,6 +1,6 @@
 curl -X POST -H "Content-Type: application/json" -d '{
   "setting_type":"greeting",
   "greeting":{
-    "text":"Hello 👋! I'"'m Wilbot, Will'"'s personal bot. Let'"'s get started when you'"'re ready"
+    "text":"Hello 👋! I'"'"'m Wilbot, Will'"'"'s personal bot. Let'"'"'s get started"
   }
 }' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=$1"
