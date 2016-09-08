88a6355516a0a168d1014f1a8203a9a3c7d26ef7
William Mora <william.r.mora@gmail.com>
2016-09-08 10:46:04 -0400

    decreased delay

--- webhook/sender_actions.js
+++ webhook/sender_actions.js
@@ -13,7 +13,7 @@ exports.markAsSeen = function(recipient, callback) {
 exports.sendTypingOn = function(recipient, message, callback) {
   let text = message.text ? message.text : message.attachment.payload.text
   typingOn(recipient, () => {
-    let millisPerCharacter = (60 * 1000) / 313
+    let millisPerCharacter = (60 * 1000) / 1500
     let delay = text.length * millisPerCharacter
     setTimeout(callback, delay)
   })
