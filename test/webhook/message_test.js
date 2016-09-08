38db3a6689c6651a125400e322bdafc370595c00
William Mora <william.r.mora@gmail.com>
2016-09-08 11:58:56 -0400

    fallback message tests

--- test/webhook/message_test.js
+++ test/webhook/message_test.js
@@ -3,7 +3,9 @@
 const should = require('should'),
       sinon = require('sinon'),
       message = require('../../webhook/message.js'),
-      postback = require('../../webhook/postback.js')
+      messenger = require('../../messenger.js'),
+      postback = require('../../webhook/postback.js'),
+      brain = require('../../brain/brain.js')
 
 describe('message', () => {
   describe('handle', () => {
@@ -38,6 +40,57 @@ describe('message', () => {
         event.message.text = '🏠'
         postbackMock = sinon.stub(postback, 'handle', (event) => {
           event.postback.payload.should.equal('home')
+          done()
+        })
+        message.handle(event)
+      })
+    })
+    describe('should send a fallback message', () => {
+      let event = {}
+      let fallbackMessage = brain.fallback_message
+      let messengerMock
+      afterEach(() => {
+        event = {}
+        messengerMock.restore()
+      })
+      it('when it is not a text message', (done) => {
+        // Message with image attachment from facebook's webhook reference
+        event = {
+          sender: {
+            id: 'message_sender_id'
+          },
+          message: {
+            mid: "mid.1458696618141:b4ef9d19ec21086067",
+            seq: 51,
+            attachments: [
+              {
+                type: "image",
+                payload: {
+                  url: "IMAGE_URL"
+                }
+              }
+            ]
+          }
+        }
+        messengerMock = sinon.stub(messenger, 'sendMessage', (recipient, message) => {
+          recipient.should.equal(event.sender)
+          message.text.should.equal(fallbackMessage)
+          done()
+        })
+        message.handle(event)
+      })
+      it('when it is not a \'home\' message', (done) => {
+        event = {
+          sender: {
+            id: 'message_sender_id'
+          },
+          message: {
+            text: 'Life, uh, finds a way'
+          }
+        }
+        messengerMock = sinon.stub(messenger, 'sendMessage', (recipient, message) => {
+          recipient.should.equal(event.sender)
+          message.text.should.equal(fallbackMessage)
           done()
         })
         message.handle(event)
